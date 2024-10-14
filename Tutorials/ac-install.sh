346faba046d2566fae1428d62a7c802591ab10cf
devwotlk <dev.wotlk@gmail.com>
2024-10-14 12:09:37 +0100

    Update ac-install.sh

--- Tutorials/ac-install.sh
+++ Tutorials/ac-install.sh
@@ -2,6 +2,7 @@
 
 # Inform the user what this script is for
 echo "This script will install AzerothCore and its dependencies, including MySQL server and additional modules."
+echo
 read -p "Do you want to continue with the installation process? (y/n): " choice
 
 # Check if the user wants to proceed
@@ -11,7 +12,7 @@ if [[ "$choice" != "y" ]]; then
 fi
 
 # Adding a wait time for user to read
-sleep 2
+sleep 5
 
 # Update package list and install dependencies
 sudo apt-get update && sudo apt-get install -y git cmake make gcc g++ clang libmysqlclient-dev \
@@ -19,41 +20,45 @@ sudo apt-get update && sudo apt-get install -y git cmake make gcc g++ clang libm
 
 # Adding a wait time for user to read
 echo "Dependencies installed."
-sleep 3
+echo
+sleep 5
 
 # Cloning AzerothCore repository
 echo "Cloning AzerothCore repository..."
+echo
 git clone https://github.com/azerothcore/azerothcore-wotlk.git --branch master --single-branch $HOME/azerothcore-source
-sleep 3
+sleep 5
 
 # Cloning Mod Eluna Lua repository
 echo "Cloning Mod Eluna Lua module..."
+echo
 git clone https://github.com/azerothcore/mod-eluna.git $HOME/azerothcore-source/modules/mod-eluna
-sleep 3
+sleep 5
 
 # Create build directory inside azerothcore-source
 echo "Creating build directory inside azerothcore-source..."
 mkdir $HOME/azerothcore-source/build
 cd $HOME/azerothcore-source/build
-sleep 3
+sleep 5
 
 # Configure the build with CMake
 echo "Configuring the build..."
 cmake ../ -DCMAKE_INSTALL_PREFIX=$HOME/azerothcore-source/env/dist/ \
     -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
     -DWITH_WARNINGS=1 -DTOOLS_BUILD=all -DSCRIPTS=static -DMODULES=static
-sleep 3
+sleep 5
 
 # Build with all available cores
 cores=$(nproc --all)
+echo
 echo "Building AzerothCore with $cores cores..."
-sleep 2
+sleep 5
 make -j $cores
 
 # Install after build is successful
 if [[ $? -eq 0 ]]; then
     echo "Build completed successfully. Installing..."
-    sleep 2
+    sleep 5
     make install
     echo "Installation complete!"
 else
