522a3cc4ab22be65a2e7ae1361da631d22e13102
devwotlk <dev.wotlk@gmail.com>
2024-10-14 12:24:16 +0100

    Update ac-install.sh

--- Tutorials/ac-install.sh
+++ Tutorials/ac-install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Inform the user what this script is for
-echo "This script will install AzerothCore and its dependencies, including MySQL server and additional modules."
+echo
+echo "This script will install AzerothCore and its dependencies, configure MySQL server, and create the required MySQL user."
 echo
 read -p "Do you want to continue with the installation process? (y/n): " choice
 
@@ -19,38 +20,63 @@ sudo apt-get update && sudo apt-get install -y git cmake make gcc g++ clang libm
     libssl-dev libbz2-dev libreadline-dev libncurses-dev mysql-server libboost-all-dev
 
 # Adding a wait time for user to read
-echo "Dependencies installed."
 echo
+echo "Dependencies installed."
+sleep 5
+
+# Configure MySQL server
+echo
+echo "Configuring MySQL server and creating user..."
+sleep 3
+sudo service mysql start
+sleep 3
+# MySQL user creation
+mysql -u root <<EOF
+DROP USER IF EXISTS 'acore'@'localhost';
+DROP USER IF EXISTS 'acore'@'%';
+CREATE USER 'acore'@'%' IDENTIFIED BY 'acore' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0;
+GRANT ALL PRIVILEGES ON *.* TO 'acore'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
+
+# Adding a wait time for user to read
+echo
+echo "MySQL user 'acore' created and configured for all hosts."
 sleep 5
 
 # Cloning AzerothCore repository
-echo "Cloning AzerothCore repository..."
 echo
-git clone https://github.com/azerothcore/azerothcore-wotlk.git --branch master --single-branch $HOME/azerothcore-source
+echo "Cloning AzerothCore repository..."
 sleep 5
+git clone https://github.com/azerothcore/azerothcore-wotlk.git --branch master --single-branch $HOME/azerothcore-source
+
 
 # Cloning Mod Eluna Lua repository
-echo "Cloning Mod Eluna Lua module..."
 echo
-git clone https://github.com/azerothcore/mod-eluna.git $HOME/azerothcore-source/modules/mod-eluna
+echo "Cloning Mod Eluna Lua module..."
 sleep 5
+git clone https://github.com/azerothcore/mod-eluna.git $HOME/azerothcore-source/modules/mod-eluna
+
 
 # Create build directory inside azerothcore-source
+echo
 echo "Creating build directory inside azerothcore-source..."
+sleep 5
 mkdir $HOME/azerothcore-source/build
 cd $HOME/azerothcore-source/build
-sleep 5
+
 
 # Configure the build with CMake
+echo
 echo "Configuring the build..."
+sleep 5
 cmake ../ -DCMAKE_INSTALL_PREFIX=$HOME/azerothcore-source/env/dist/ \
     -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
     -DWITH_WARNINGS=1 -DTOOLS_BUILD=all -DSCRIPTS=static -DMODULES=static
-sleep 5
+
 
 # Build with all available cores
 cores=$(nproc --all)
-echo
 echo "Building AzerothCore with $cores cores..."
 sleep 5
 make -j $cores
@@ -58,7 +84,7 @@ make -j $cores
 # Install after build is successful
 if [[ $? -eq 0 ]]; then
     echo "Build completed successfully. Installing..."
-    sleep 5
+    sleep 2
     make install
     echo "Installation complete!"
 else
